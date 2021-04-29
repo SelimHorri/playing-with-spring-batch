f115a1193aa23e11dd74ec80548cfde5df964950
SelimHorri <horri.slim@gmail.com>
2021-04-29 17:48:08 +0100

    init

---
+++ src/main/java/com/selimhorri/pack/PlayingWithSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.selimhorri.pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayingWithSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayingWithSpringBatchApplication.class, args);
+	}
+
+}
