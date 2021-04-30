0743b49f9918571e93b84feac137b24187eb19cc
SelimHorri <horri.slim@gmail.com>
2021-04-30 02:04:53 +0100

    create db migrations with flyway

---
+++ src/main/resources/db/migration/V1__create_employees_table.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE employees (
+	id INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	fname VARCHAR(255),
+	lname VARCHAR(255),
+	email VARCHAR(255),
+	image_url VARCHAR(255),
+	hiredate DATE
+);
---
+++ src/main/resources/db/migration/V2__insert_employees_table.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO employees (fname, lname, email, image_url, hiredate) VALUES
+('selim', 'horri', 'slim__horri@hotmail.com', 'https://bootdey.com/img/Content/avatar/avatar7.png', '2019-04-15'),
+('omar', 'derouiche', 'omar.derouiche@gmail.com', 'https://bootdey.com/img/Content/avatar/avatar6.png', '2020-04-01'),
+('amine', 'ladjimi', 'amine.ladjimi@yahoo.fr', 'https://bootdey.com/img/Content/avatar/avatar5.png', '2021-04-01');
